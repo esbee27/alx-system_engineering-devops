1f757f7bdb9a8f814d267f1f63fa1fd03f452425
Blessing Ogor <ogorblessing96@gmail.com>
2024-07-29 17:15:24 -0700

    Task 9 done

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,20 +5,20 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(userId))
     name = user.json().get('name')
 
     todos = requests.get("https://jsonplaceholder.typicode.com/todos")
-    totatlTask = 0
+    totalTask = 0
     completed = 0
 
     for task in todos.json():
-        if task.get('user_id') == int(user_id):
+        if task.get('userId') == int(userId):
             totalTask += 1
             if task.get('completed'):
                 completed += 1
 
     print('Employee {} is done with tasks({}/{}):'.format(name, completed, totalTask))
-    print('\n'.join(["\t" + task.get('title') for task in todos.json()
-        if task.get('user_id') == int(user_id) and task.get('completed')]))
+    print('\n'.join(["\t " + task.get('title') for task in todos.json()
+        if task.get('userId') == int(userId) and task.get('completed')]))
