116ebcc9bc04f71119272e0efeb0a35a5ebd1b3d
Blessing Ogor <ogorblessing96@gmail.com>
2024-07-31 12:00:36 -0700

    Task 3 done

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """A Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress"""
 
-import json
-import requests
-import sys
-
 if __name__ == "__main__":
+
+    import json
+    import requests
+    import sys
 
     users = requests.get("https://jsonplaceholder.typicode.com/users")
     users = users.json()
