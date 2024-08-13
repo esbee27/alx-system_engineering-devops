42bb743d5a885971e15d652fd31a6b2341594cbf
Blessing <ogorblessing96@gmail.com>
2024-08-13 15:59:04 +0000

    Task 0 done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """A script thatreturns the number of subscribers"""
-
 import requests
+
 
 def number_of_subscribers(subreddit):
     """A function that returns number of subcribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     response = requests.get(url, allow_redirects=False)
-    if response.status_code == 200:
-        return response.json()['data']['subcribers']
-    return 0
+    if response.status_code == 404:
+        return 0
+    result = response.json().get("data")
+    return result.get("subcribers")
