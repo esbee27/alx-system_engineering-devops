c9a437f686b7495055bcd26c0314412e0a969603
Blessing <ogorblessing96@gmail.com>
2024-08-14 13:25:16 +0000

    Web stack debuin

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Creates a manidest that fixes typos of a filename
 exec { 'fix_typo':
-  command => 'mv /var/www/html/wp_includes/class_wp_locale.php /var/www/html/wp_includes/class_wp_locale.phpp',
+  command => 'mv /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
   path => '/bin/'
   }
