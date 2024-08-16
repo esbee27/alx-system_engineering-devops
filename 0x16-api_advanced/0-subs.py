0984c946df12ca09d838cee646734e9b4c8b53bf
Blessing <ogorblessing96@gmail.com>
2024-08-15 22:02:07 -0700

    Done

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 def number_of_subscribers(subreddit):
     """A function that returns number of subcribers for a given subreddit"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers={"User-Agent": "My-Agent"})
+    response = requests.get(url, allow_redirects=False, headers={"User-Agent": "Chrome/127.0.0"})
     if response.status_code >= 300:
         return 0
 
