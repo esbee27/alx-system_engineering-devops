f7278b0ad4f0be53f994f61032a957e3f7cfd695
Blessing Ogor <ogorblessing96@gmail.com>
2024-08-20 16:12:20 +0000

    Done

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Creates a manidest that fixes typos of a filename
 exec { 'fix_wordpress':
   command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-  path => 'usr/local/bin/:/bin/'
+  path    => 'usr/local/bin/:/bin/'
   }
